9cbcb8cd3866ed2fe5134ca9b087f7623b8fdbad
Retno Suci Wulandari <retnosw389@gmail.com>
2018-05-12 15:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var height = "74"
 module.exports = { name, height
 }
 
-if (name === "Joe") {
+if (name === "John") {
     console.log("The name is correct")
 } else {
-    console.log("Expected the name to be san")
+    console.log("Expected the name to be John")
 }
